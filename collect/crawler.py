3108ade3a1732f770e14619137c04cba568efdf9
SINAEEE <kimshin1227@naver.com>
2018-06-14 15:02:30 +0900

    analysis PD commit

--- collect/crawler.py
+++ collect/crawler.py
@@ -1,10 +1,40 @@
 
+import os
+import json
 from .api import api
 
 RESULT_DIRECTORY = '__results__/crawling'
 
 
-def preprocess_post(item): #데이터전처리 : 공개할필요없는 함수
+def preprocess_foreign_visitor(data):
+
+    #필요없는 데이터 지우기
+    del data['ed']
+    del data['edCd']
+    del data['rnum']
+
+    #나라코드
+    data['country_code'] = data['natCd']
+    del data['natCd']
+
+    #나라 이름
+    data['country_Name'] = data['natKorNm'].replace(' ','')
+    del data['natKorNm']
+
+    #방문자 수
+    data['visit_counter'] = data['num']
+    del data['num']
+
+    #년월
+    if 'ym' not in data:
+        data['date'] = ''
+    else:
+        data['date'] = data['ym']
+        del data['ym']
+
+
+"""
+def  preprocess_tourspot_visitor(item): #데이터전처리 : 공개할필요없는 함수
     #내국인 수
     if 'addrCd' not in item:
         item['count_locals'] = 0
@@ -40,6 +70,28 @@ def preprocess_post(item): #데이터전처리 : 공개할필요없는 함수
         item['restrict2'] = 0
     else:
         item['restrict2'] = item['gungu']
+"""
+
+
+def crawlling_foreigner_visitor(country, start_year, end_year):
+
+    results = []
+
+    for year in range(start_year,end_year+1):
+        for month in range(1,13):
+            #print("fetching.." + country[0] + ":" + str(year) + "-" + str(month))
+                data = api.pd_fetch_foreigner_visitor(country[1],year,month)
+                #print(data)
+                if data is None:
+                    continue
+
+                preprocess_foreign_visitor(data)
+                #results += data
+                results.append(data)
+
+    #save data to file
+    print(results)
+
 
 
 
@@ -50,25 +102,14 @@ def crawlling_tourspot_visitor(district, start_year, end_year):
 
     for items in api.pd_fetch_tourspot_visitor(district, start_year, end_year):
         for item in items:
+            preprocess_post(item)
 
-"""
-def crawling(pagename, since, until):
-    results = []
-    filename = '%s_%s_%s_%s.json' % (RESULT_DIRECTORY,pagename,since,until)
+        results += items
 
-    for posts in api.fb_fetch_posts(pagename, since, until):
-        for post in posts: #개별전처리(50개씩받아서 하나씩)
-            preprocess_post(post)
-
-        results += posts
-
-    #save results to file(저장/적재)
-    #outfile = open(filename, 'w', encoding='utf-8')
+    # save results to file(저장/적재)
     with open(filename, 'w', encoding='utf-8') as outfile:
         json_string = json.dumps(results, indent=4, sort_keys=True, ensure_ascii=False)
         outfile.write(json_string)
 
-
 if os.path.exists(RESULT_DIRECTORY) is False:
     os.makedirs(RESULT_DIRECTORY)
-"""
--- main.py
+++ main.py
@@ -2,4 +2,21 @@
 
 import collect
 
+
 if __name__ == '__main__':
+
+    # 데이터 수집(collection)
+    """
+    collect.crawlling_tour_spot_visitor(district='서울특별시',
+                                       start_year=2017,
+                                       end_year=2017)
+    """
+
+    for country in [('중국',112),('일본',130),('미국',275)]:
+            collect.crawlling_foreigner_visitor(country,2017,2017)
+
+
+    #데이터 분석(analysis)
+    #데이터 시각화 (visualization)
+
+
